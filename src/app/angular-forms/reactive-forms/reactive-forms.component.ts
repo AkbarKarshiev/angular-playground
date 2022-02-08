072e93311e5ca1adcdda9a20403c0fa965843efb
AkbarKarshiev <50545784+AkbarKarshiev@users.noreply.github.com>
2022-02-08 20:07:24 +0500

    Angular forms: FormBuilder

--- src/app/angular-forms/reactive-forms/reactive-forms.component.ts
+++ src/app/angular-forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-forms',
@@ -13,26 +13,79 @@ export class ReactiveFormsComponent implements OnInit {
     { id: '3', name: "England" },
   ]
 
-  contactForm = new FormGroup({
-    firstName: new FormControl({ value: 'Akbar', disabled: true}),
-    lastName: new FormControl('', [Validators.required, Validators.minLength(10)]),
-    email: new FormControl(),
-    gender: new FormControl(),
-    isMarried: new FormControl(),
-    country: new FormControl(),
-    address: new FormGroup({
-      city: new FormControl(),
-      street: new FormControl(),
-      pinCode: new FormControl()
+  // contactForm = new FormGroup({
+  //   firstName: new FormControl({ value: 'Akbar', disabled: true}),
+  //   lastName: new FormControl('', [Validators.required, Validators.minLength(10)]),
+  //   email: new FormControl(),
+  //   gender: new FormControl(),
+  //   isMarried: new FormControl(),
+  //   country: new FormControl(),
+  //   address: new FormGroup({
+  //     city: new FormControl(),
+  //     street: new FormControl(),
+  //     pinCode: new FormControl()
+  //   })
+  // })
+
+  contactForm = this.formBuilder.group({
+    firstName: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(15)]],
+    lastName: [{value: 'test', disabled: true}],
+    email: ['', [Validators.required, Validators.email]],
+    gender: ['', [Validators.required]],
+    isMarried: ['', [Validators.required]],
+    country: ['', [Validators.required]],
+    address: this.formBuilder.group({
+      city: ['', [Validators.required]],
+      street: ['', [Validators.required]],
+      pinCode: ['', [Validators.required]],
     })
   })
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get gender() {
+    return this.contactForm.get('gender');
+  }
+
+  get isMarried() {
+    return this.contactForm.get('isMarried');
+  }
+
+  get country() {
+    return this.contactForm.get('country');
+  }
+
+  get city() {
+    return this.contactForm.get("address").get('city');
+  }
+
+  get street() {
+    return this.contactForm.get("address").get('street');
+  }
+
+  get pinCode() {
+    return this.contactForm.get("address").get('pinCode');
+  }
+
   onSubmit() {
-    console.log(this.contactForm.value)
+    console.log(this.contactForm.controls);
+
   }
 }
